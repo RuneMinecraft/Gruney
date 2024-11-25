ea485ef3cc4628b5f844558f75de9db9b356b4d9
dank1234 <139914667+dank1234@users.noreply.github.com>
2024-11-25 23:48:39 +0000

    dev

--- src/main/java/net/runemc/plugin/scripting/commands/LoadScript.java
+++ src/main/java/net/runemc/plugin/scripting/commands/LoadScript.java
@@ -1,18 +1,23 @@
 package net.runemc.plugin.scripting.commands;
 
+import net.neoforged.srgutils.IMappingFile;
+import net.runemc.utils.command.Cmd;
+import net.runemc.utils.command.ICommand;
+import net.runemc.utils.server.ServerType;
+import net.runemc.utils.wrapper.message.Message;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-public class LoadScript implements CommandExecutor {
+@Cmd(
+        names = {"load-script", "loadscript"},
+        perms = "rune.admin"
+)
+public class LoadScript extends ICommand {
+
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (!(strings.length > 0))return false;
-
-        //load here
-
-
-        return false;
+    public void execute(CommandSender sender, String[] args) {
+        Message.create(sender, "hi").send();
     }
 }
